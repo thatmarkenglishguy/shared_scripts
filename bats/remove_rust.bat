4b208b121d95cc87828404f8f688d6ada49e1805
Mark English <thatmarkenglishguy@gmail.com>
2023-01-11 02:48:56 +0000

    Corrected remove rust.

--- bats/remove_rust.bat
+++ bats/remove_rust.bat
@@ -1,5 +1,10 @@
-@set OLD_RUST_PATH="%PATH%"
 @set RUST_PATH=%USERPROFILE%\.cargo\bin
 
 @set PATH| findstr /C:"%RUST_PATH%"
-@if %ERRORLEVEL% NEQ 0 @set PATH="%PATH%;%RUST_PATH%"
+@if %ERRORLEVEL% NEQ 0 @goto got_rust
+
+:got_rust
+@if defined OLD_RUST_PATH @set PATH=%OLD_RUST_PATH%
+@set OLD_RUST_PATH=
+
+:exit
