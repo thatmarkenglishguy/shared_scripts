e3593920e25f65377e43aa624bdde13715f119a5
Mark English <thatmarkenglishguy@gmail.com>
2025-04-29 10:41:48 +0100

    setup_local Install docker.

--- setup_local.sh
+++ setup_local.sh
@@ -313,6 +313,31 @@ else
   fi
 fi
 
+# Sort out docker and kvm tooling
+# See https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository
+# ---
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install --assume-yes ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+# ---
+
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove --assume-yes $pkg; done
+
+# This includes 'kvmok'
+sudo apt install --assume-yes cpu-checker
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 if [ -f "${script_dir}/dotfiles/setup_local_bashrc.sh" ]
 then
   echo 'Setting up bashrc.' >&2
