c41f1e43e4fdad16247844807fb645ac2eaa773d
Mark English <thatmarkenglishguy@gmail.com>
2025-12-02 10:45:39 +0000

    add_rust Fix RUST_PATH check.

--- bats/add_rust.bat
+++ bats/add_rust.bat
@@ -1,5 +1,6 @@
 @set OLD_RUST_PATH=%PATH%
 @set RUST_PATH=%USERPROFILE%\.cargo\bin
 
-@set PATH| findstr /C:"%RUST_PATH%"
+:: For some reason the . in .cargo needs to be escaped
+@set PATH| findstr /C:"%RUST_PATH:.=\.%" >NUL
 @if %ERRORLEVEL% NEQ 0 @set PATH=%PATH%;%RUST_PATH%
